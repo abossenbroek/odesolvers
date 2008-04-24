b9eda04924fcf6a063a724e4722c5f35bd9dc84c
Anton Bossenbroek <anton@omhp-uva-20-115.wireless.uva.nl>
2008-04-24 22:15:21 +0200

    Added GPLv3 copyright notice.

--- diffusion.c
+++ diffusion.c
@@ -1,3 +1,23 @@
+/* 
+ * This program solves the pde for the 2D diffusion equation using MPI-v2 and
+ * SSE2.
+ *
+ * Copyright (C) 2008  Anton Bossenbroek <abossenb@science.uva.nl>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <mpi.h>
 #include <math.h>
 #include <sysexits.h>
--- diffusion.h
+++ diffusion.h
@@ -1,3 +1,23 @@
+/* 
+ * This program solves the pde for the 2D diffusion equation using MPI-v2 and
+ * SSE2.
+ *
+ * Copyright (C) 2008  Anton Bossenbroek <abossenb@science.uva.nl>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef _DIFFUSION_H_
 #define _DIFFUSION_H_
 
@@ -6,21 +26,28 @@
 #define NUM_PARAMS 7
 
 typedef struct {
-	double dx;
-	double dt;
-	double D;
+	float dx;
+	float dt;
+	float D;
 	int	 ntotal;
-	int	 ttotal;
+	long	 ttotal;
 	int	 l;
 	int	 h;
 } pparams;
 
 int pparams_blength[NUM_PARAMS] = {1, 1, 1, 1, 1, 1, 1};
 
-MPI_Datatype pparams_type[NUM_PARAMS] = {MPI_DOUBLE, MPI_DOUBLE, MPI_DOUBLE, 
-	MPI_INT, MPI_INT, MPI_INT, MPI_INT};
+MPI_Datatype pparams_type[NUM_PARAMS] = {MPI_FLOAT, MPI_FLOAT, MPI_FLOAT, 
+	MPI_INT, MPI_LONG, MPI_INT, MPI_INT};
 
 enum { X_COORD,
-	Y_COORD };
+	Y_COORD,
+	X_UP_TAG,
+   X_DOWN_TAG,
+	Y_LEFT_TAG,
+	Y_RIGHT_TAG
+};
 #endif //  _DIFFUSION_H_
+
+/* vim: set spell spelllang=en:cindent:tw=80:et*/
 
